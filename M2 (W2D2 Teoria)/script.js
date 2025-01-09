d6d7d7735a703c44e513e8d80304c0357da9dcaf
Chiara Indirli <chiaraindirli2@gmail.com>
2025-01-09 21:02:51 +0100

    esercizio in live effettuato

---
+++ M2 (W2D2 Teoria)/script.js
@@ -0,0 +1,26 @@
+/* const studentsArray = [
+    { name: 'Mario', surname: 'Rossi', age: 20 },
+    { name: 'Francesco', surname: 'Bianchi', age: 65 },
+];
+
+studentsArray.push({ name: 'Barbara', surname: 'Verdi', age: 30 });
+
+for (let student of studentsArray) {
+    console.log(`Mi chiamo ${student.name} e ho ${student.age} anni.`);
+}
+
+const stringArray = ["pippo", "topolino", "minnie"];
+stringArray.pop ();
+stringArray.unshift("paperino");
+console.log(stringArray);
+
+/*console.log(stringArray.length);*/
+
+const numberArray = [1, 5, 7, 9, 12, 87];
+const isNumberIncluded = numberArray.includes(112)
+if(isNumberIncluded) {
+console.log('il numero è presente');
+} else {
+    console.log('il numero non è presente');
+}
+
