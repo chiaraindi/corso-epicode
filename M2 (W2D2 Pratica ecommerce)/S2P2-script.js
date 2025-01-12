6695937b246745ea6ed43e99bbffedbb41dff369
Chiara Indirli <chiaraindirli2@gmail.com>
2025-01-12 17:12:27 +0100

    esercizio done

--- M2 (W2D2 Pratica ecommerce)/S2P2-script.js
+++ M2 (W2D2 Pratica ecommerce)/S2P2-script.js
@@ -16,20 +16,52 @@ const amy = {
   isAmbassador: false,
 }
 
-const prices = [34, 5, 2]
+const prices = [34, 105, 102]
 const shippingCost = 50
 let utenteCheEffettuaLAcquisto = amy //cambia il valore qui per provare se il tuo algoritmo funziona!
 
-/* Inizio esercizio: */
+/* Facciamo la somma del carrello */
 
 const totalPrices = prices.reduce ((sum, prices) => sum + prices, 0);
 console.log("Costo totale del carrello (senza sconto):", totalPrices);
 
+/* Applichiamo lo sconto */
+
 let discountedTotal = totalPrices;
 
 if (utenteCheEffettuaLAcquisto.isAmbassador) {
-  discountedTotal = totalPrices * 0,7; //sconto del 30%
+  discountedTotal = totalPrices * 0.7; //sconto del 30%
 } else {
-  discountedTotal = totalPrices; // Nessuno sconto applicato
+  discountedTotal = totalPrices; //nessuno sconto applicato
 }
 console.log("Costo finale del carrello:", discountedTotal);
+
+/* Calcoliamo i costi di spedizione */
+
+let finalTotal = discountedTotal;
+
+if (discountedTotal <= 100) {
+  finalTotal += shippingCost; //spese di spedizione aggiunte per spese minori di 100 euro
+} else {
+  console.log("La spedizione è gratuita!");
+}
+
+console.log("Costo totale del carrello (inclusa spedizione):", finalTotal);
+
+/* gestione ambassador */
+
+const users = [];
+users.push(marco, paul, amy);
+
+users.forEach(user => {
+  if (user.isAmbassador) {
+    console.log(`${user.name} ${user.lastName} è un ambassador`);
+  } else {
+    console.log(`${user.name} ${user.lastName} NON è un ambassador`);
+  }
+});
+
+/* array con solo ambassador" */
+
+const ambassadors = users.filter(user => user.isAmbassador);
+console.log("Ambassadors", ambassadors);
