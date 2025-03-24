4e847059514e9cd98a053776ff9fde17579c9786
Chiara Indirli <chiaraindirli2@gmail.com>
2025-03-24 19:38:31 +0100

    update spinner

--- M4 (W1D4) spotify/script.js
+++ M4 (W1D4) spotify/script.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
     resultsContainer.innerHTML = `
       <div class="d-flex justify-content-center">
         <div class="spinner-border text-primary" role="status">
-          <span class="sr-only">Loading...</span>
+          <span class="sr-only"></span>
         </div>
       </div>
     `;
